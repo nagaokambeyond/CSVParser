79ea68298ead9ed4a7be1fc4d4388e610208c876
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2023-05-16 00:57:25 +0900

    initial
    最小限のソース
    build.gradle
    gradleから実行

---
+++ src/main/java/CSVParser.java
@@ -0,0 +1,119 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CSVParser {
+
+    String oneRes;
+    private String line = "";
+    private int nbLines = 0;
+    private final BufferedReader reader;
+
+    public CSVParser(BufferedReader reader) {
+        this.reader = reader;
+    }
+
+    public String[] splitLine() throws Exception {
+        nbLines = 0;
+        var al = new ArrayList<String>();
+        line = nextLine();
+        if (line == null)
+            return null;
+
+        nbLines = 1;
+        int pos = 0;
+
+        while (pos < line.length()) {
+            pos = findNextComma(pos);
+            al.add(oneRes);
+            pos++;
+        }
+
+        if (line.length() > 0 && line.charAt(line.length() - 1) == ',') {
+            al.add("");
+        }
+
+        return al.toArray(new String[] {});
+    }
+
+    private int findNextComma(int p) throws Exception {
+        char c;
+        int i;
+        oneRes = "";
+        c = line.charAt(p);
+
+        // empty field
+        if (c == ',') {
+            oneRes = "";
+            return p;
+        }
+
+        // not escape char
+        if (c != '"') {
+            i = line.indexOf(',', p);
+            if (i == -1)
+                i = line.length();
+            oneRes = line.substring(p, i);
+            return i;
+        }
+
+        // start with "
+        p++;
+
+        var sb = new StringBuilder(200);
+        while (true) {
+            c = readNextChar(p);
+            p++;
+
+            // not a "
+            if (c != '"') {
+                sb.append(c);
+                continue;
+            }
+
+            // ", last char -> ok
+            if (p == line.length()) {
+                oneRes = sb.toString();
+                return p;
+            }
+
+            c = readNextChar(p);
+            p++;
+
+            // "" -> just print one
+            if (c == '"') {
+                sb.append('"');
+                continue;
+            }
+
+            // ", -> return
+            if (c == ',') {
+                oneRes = sb.toString();
+                return p - 1;
+            }
+
+            throw new Exception("Unexpected token found");
+        }
+    }
+
+    private char readNextChar(int p) throws Exception {
+        if (p == line.length()) {
+            String newLine = reader.readLine();
+            if (newLine == null)
+                throw new Exception("Error occurred while parsing");
+            line += "\n" + newLine;
+            nbLines++;
+        }
+        return line.charAt(p);
+    }
+
+    public String nextLine() throws IOException {
+        String result = "";
+        do {
+            result = reader.readLine();
+            if (result == null)
+                return null;
+        } while (result.trim().equals(""));
+        return result;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = null;
+        try {
+            String doc = """
+                a,a  ab,c,d a
+                ,1 a
+                1,\s
+                a,
+                1,"v ""a v\"""";
+
+            System.out.println("String to be parsed = " + doc);
+            reader = new BufferedReader(new StringReader(doc));
+            CSVParser parser = new CSVParser(reader);
+            String[] res;
+
+            while ((res = parser.splitLine()) != null) {
+                for (String r : res) {
+                    System.out.println("Token Found [" + r + "] \n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null){
+                reader.close();
+            }
+        }
+    }
+}
